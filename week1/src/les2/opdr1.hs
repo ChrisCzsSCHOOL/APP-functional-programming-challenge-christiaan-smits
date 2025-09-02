abfe8122960072d20a31ad5465a4e41c1af41dfd
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2025-09-02 15:17:31 +0200

    Ja dit lukte dus niet

--- week1/src/les2/opdr1.hs
+++ week1/src/les2/opdr1.hs
@@ -1,8 +1,9 @@
+import Data.Array
+import Control.Arrow (ArrowLoop(loop))
+
+lijst :: Array Int Int
+lijst = listArray (1, 5) [1,2,3,4,5]
+
 main :: IO()
 
 main = do
-    lijst :: array (1,5)
-
-    lijst 
-
-
