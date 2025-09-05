ff651f81cbb50739e3887c08d1d0cb62ad4aaea8
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2025-09-05 13:40:11 +0200

    Palindrome checker✅

--- week1/src/les3/Les3Opdr4.java
+++ week1/src/les3/Les3Opdr4.java
@@ -1,20 +1,14 @@
 public class Les3Opdr4 {
     public static void main(String[] args) {
-
+        String n = "ekitike";
+//        String n = "racecar";
+//        String n = "hello";
+        System.out.println(isPalindrome(n, 0));
     }
 
-    public static boolean isPalindrome(String n){
-
-        // eerste letter checken
-
-        // tweede letter checken
-
-        // Niet hetzelfde? Dan false returnen
-
-        // Zelfde dan de volgende letters checken
-
-
-
-        return false;
+    public static boolean isPalindrome(String n, int index) {
+        if (index == n.length() - 1) return true; // Check of we aan het einde van de string zijn
+        if (n.charAt(index) == n.charAt(n.length() - (1 + index) )) return isPalindrome(n, index + 1); // return het resultaat van de recursie
+        return false; // Als de karakters niet gelijk zijn, return false
     }
 }
