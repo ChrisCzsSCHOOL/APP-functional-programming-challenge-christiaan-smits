664335fde361900a8606f4179a3c15ffb1237610
Christiaan Smits <150244385+ChrisCzsSCHOOL@users.noreply.github.com>
2025-09-09 14:26:58 +0200

    Dankjewel mienier

---
+++ week2/les2/Opdr6.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Opdr6 {
+
+    public static void main(String[] args) {
+
+
+//        Integer[] array = {28, 4, 9, 2, 6, 15, 21, 1};
+        String [] array = {"bombo", "clat", "agada"};
+
+        System.out.println(Arrays.toString(insertionSort(array)));
+    }
+
+    public static <T extends Comparable> T[] insertionSort(T[] n) {
+        // kijk in array
+        for (int i = 0; i < n.length; i++) {
+            // Base case
+            if (i + 1 == n.length) return n;
+
+            // is rechts kleiner dan links?
+            if (n[i].compareTo(n[i + 1]) > 0) {
+
+                // Draai om
+                T temp = n[i + 1];
+                n[i + 1] = n[i];
+                n[i] = temp;
+
+                // Recursive call lol?
+                n = insertionSort(n);
+            }
+        }
+        return n;
+    }
+}
